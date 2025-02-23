4c2556aad509049f21d9b3cd3d06ab5dc49b84b1
FraiFish <trashell.cn@gmail.com>
2025-02-23 00:55:17 -0500

    Added new song

--- js/rand.js
+++ js/rand.js
@@ -1,6 +1,6 @@
 ﻿var path = '/media/songs/',
-    files = ['arc.mp3', 'etc.mp3', 'pop.mp3'],
-    songnames = ["Live Fast Die Young(Extended Mix) - Anubasu-anubasu", 'Close to the Heavens - Masayoshi Soken','㋰責任集合体 - マサラダ']
+    files = ['arc.mp3', 'etc.mp3', 'pop.mp3', 'gravekeeper.mp3'],
+    songnames = ["Live Fast Die Young(Extended Mix) - Anubasu-anubasu", 'Close to the Heavens - Masayoshi Soken','㋰責任集合体 - マサラダ', "躯樹の墓守 - 隣の庭は青い(庭師+Aoi)"]
     i = Math.floor(Math.random()*files.length);
 var url = (path+files[i]);
 
